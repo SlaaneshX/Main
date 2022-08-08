23a798c91d021660ec5302da7e90feca5fd049c1
Alexander <tumakov.a@yandex.ru>
2022-08-08 13:55:58 +0300

    fix

--- Likes/src/main/kotlin/Main.kt
+++ Likes/src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    val likes = 721
+    val likes = 711
 
     print(likes.toString() + " " + getLikesString(likes))
 }
@@ -7,10 +7,10 @@ fun main(args: Array<String>) {
 fun getLikesString(num: Int): String? {
     val secondFromEnd = num % 100 / 10
     return if (secondFromEnd == 1) {
-        "лайков"
+        "людям"
     } else when (num % 10) {
-        1 -> "лайк"
-        2, 3, 4 -> "лайка"
-        else -> "лайков"
+        1 -> "человеку"
+        2,3,4,5,6,7,8,9 -> "людям"
+        else -> "людям"
     }
 }
