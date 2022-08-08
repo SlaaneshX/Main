b41cfb7af7f561f88fc1e353832081cc350b04c6
Alexander <tumakov.a@yandex.ru>
2022-08-08 13:36:16 +0300

    fix

--- Immutability/src/main/kotlin/main.kt
+++ Immutability/src/main/kotlin/main.kt
@@ -2,6 +2,6 @@ fun main() {
     val amount = 100000
     val taxMin = 3500
     val taxPercent = 0.75
-    var finalTax = if (amount * taxPercent < taxMin) taxMin else amount * taxPercent / 100
+    var finalTax = if (amount * taxPercent / 100 < taxMin) taxMin else amount * taxPercent / 100
     println(finalTax)
 }
