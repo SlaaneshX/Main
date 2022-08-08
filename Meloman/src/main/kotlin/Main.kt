38b0bbdd853dc27b588ce66d6eab06f91d411d35
Alexander <tumakov.a@yandex.ru>
2022-08-08 13:18:22 +0300

    fix

--- Meloman/src/main/kotlin/Main.kt
+++ Meloman/src/main/kotlin/Main.kt
@@ -1,16 +1,8 @@
 fun main() {
     val melomanStatus = true
     val itemPrice = 100.0
-    val noDiscount = 1000
-    val startDiscount = 100
-    val secondDiscount = 10000
-    val maxDiscountPercent = 0.05
-
     val itemsCount = 3
-
-
-    var total = itemsCount * itemPrice
-
+    
     calculateDiscount(itemPrice, itemsCount, melomanStatus)
 }
 
@@ -18,7 +10,7 @@ fun melomanDiscount(meloman: Boolean, total: Double) {
     var summ = total
     if (meloman) {
         summ -= total * 0.01
-        println("Сумма покупки с учетом скидки постоянного клиента: $summ")
+        println("РЎСѓРјРјР° РїРѕРєСѓРїРєРё СЃ СѓС‡РµС‚РѕРј СЃРєРёРґРєРё РїРѕСЃС‚РѕСЏРЅРЅРѕРіРѕ РєР»РёРµРЅС‚Р°: $summ")
     }
 }
 
@@ -26,14 +18,14 @@ fun calculateDiscount(price: Double, count: Int, melomanStatus: Boolean) {
     var total = count * price
     if (total > 1000 && total < 10000) {
         total -= 100
-        println("Сумма покупки с учетом скидки: $total")
+        println("РЎСѓРјРјР° РїРѕРєСѓРїРєРё СЃ СѓС‡РµС‚РѕРј СЃРєРёРґРєРё: $total")
         melomanDiscount(melomanStatus, total)
     } else if (total > 10000) {
         total -= total * 0.05
-        println("Сумма покупки с учетом скидки: $total")
+        println("РЎСѓРјРјР° РїРѕРєСѓРїРєРё СЃ СѓС‡РµС‚РѕРј СЃРєРёРґРєРё: $total")
         melomanDiscount(melomanStatus, total)
     } else {
-        println("Сумма покупки: $total")
+        println("РЎСѓРјРјР° РїРѕРєСѓРїРєРё: $total")
         melomanDiscount(melomanStatus, total)
     }
 }
