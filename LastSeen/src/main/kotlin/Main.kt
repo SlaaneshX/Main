0567642fc3e3a619f5928820da8eeaf25f6c3cb2
Alexander <tumakov.a@yandex.ru>
2022-08-09 17:46:52 +0300

    last seen

---
+++ LastSeen/src/main/kotlin/Main.kt
@@ -0,0 +1,39 @@
+fun main() {
+    val lastSeenTime = 0
+
+    print(agoToText(60 * 60 * 3))
+}
+
+fun agoToText(seconds: Int): String {
+    return when {
+        seconds in 0..60 -> "только что"
+        seconds in 61..60 * 60 -> "был(а) в сети ${seconds / 60} ${timeEnding(seconds / 60)} назад"
+        seconds in 60 * 60 + 1..60 * 60 * 24 -> "был(а) в сети ${(seconds / 60) / 60} ${timeEndingHour((seconds / 60) / 60)} назад"
+        seconds in 60 * 60 * 24 + 1..60 * 60 * 24 * 2 -> "сегодня"
+        seconds in 60 * 60 * 24 * 2 + 1..60 * 60 * 24 * 3 -> "вчера"
+        seconds > 60 * 60 * 24 * 3 + 1 -> "давно"
+        else -> "[default]"
+    }
+}
+
+fun timeEnding(min: Int): String {
+    val time = min % 100 / 10
+    return if (time == 1) {
+        "минут"
+    } else when (min % 10) {
+        1 -> "минуту"
+        2, 3, 4 -> "минуты"
+        else -> "минут"
+    }
+}
+
+fun timeEndingHour(min: Int): String {
+    val time = min % 100 / 10
+    return if (time == 1) {
+        "часов"
+    } else when (min % 10) {
+        1 -> "час"
+        2, 3, 4 -> "часа"
+        else -> "часов"
+    }
+}
